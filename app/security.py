c9df6855b7f5ded6cd1f4f40f27d69c23f16990f
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-12-18 16:46:02 +0530

    refresh token logic implemented

--- app/security.py
+++ app/security.py
@@ -9,6 +9,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = os.getenv('ALGORITHM')
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv('REFRESH_TOKEN_EXPIRE_DAYS'))
 
 
 pwd_context = CryptContext(schemes=['argon2'], deprecated='auto')
@@ -27,4 +28,13 @@ def create_access_token(data: dict):
         {'exp': expire}
     )
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update(
+        {'exp': expire, 'type': 'refresh'}
+    )
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
 
