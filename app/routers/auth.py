76edbebd274a1351b5a4ae2dc7cfeb0b183f0ea7
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-12-18 16:46:26 +0530

    refresh route added for new access token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,9 +3,23 @@ from sqlalchemy.orm import Session
 from app.schemas import UserCreate, UserResponse, TokenResponse, LoginRequest
 from app.crud import create_user, get_users, get_user_by_email
 from app.database import get_db
-from app.security import verify_password, create_access_token
+from app.security import verify_password, create_access_token, create_refresh_token
 from fastapi import HTTPException, status
 from app.dependencies import get_current_user
+from fastapi import Request, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.crud import get_user_by_email
+from jose import jwt, JWTError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
 
 router = APIRouter(prefix='/api/auth')
 
@@ -34,6 +48,7 @@ def UserLogin(user: LoginRequest, response: Response, db: Session = Depends(get_
         )
 
     access_token = create_access_token({"sub": db_user.email})
+    refresh_token = create_refresh_token({"sub": db_user.email})
 
     response.set_cookie(
         key='access_token',
@@ -41,15 +56,54 @@ def UserLogin(user: LoginRequest, response: Response, db: Session = Depends(get_
         httponly=True,
         secure=False,
         samesite='lax',
-        max_age=1800 #30 minutes
+        max_age=24*60*60 #1 day
     )
 
-    return {"access_token": access_token}
+    response.set_cookie(
+        key='refresh_token',
+        value=refresh_token,
+        httponly=True,
+        secure=False,
+        samesite='lax',
+        max_age=7*24*60*60 #7 days
+
+    )
+
+    return {"access_token": access_token, "refresh_token": refresh_token}
 
 
 @router.get("/user/me", response_model = UserResponse)
 def get_me(current_user = Depends(get_current_user)):
     return current_user
+
+
+@router.post("/refresh")
+def refresh_token_reload(request: Request, response: Response, db: Session = Depends(get_db)):
+    token = request.cookies.get("refresh_token")
+    if not token:
+        raise HTTPException(status_code=401, detail="No refresh token found")
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        if payload.get("type") != "refresh":
+            raise HTTPException(status_code=401, detail="Invalid token type")
+        email = payload.get("sub")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid refresh token")
+
+    # Create new access token
+    new_access_token = create_access_token({"sub": email})
+
+    response.set_cookie(
+        key="access_token",
+        value=new_access_token,
+        httponly=True,
+        secure=False,   # True in production
+        samesite="lax",
+        max_age=24*60*60
+    )
+
+    return {"message": "Access token refreshed"}
 
 
 
