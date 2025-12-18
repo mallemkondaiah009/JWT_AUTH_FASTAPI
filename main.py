f55adb7b7637b5dad61b9a9bd2de3f32daee4c27
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-12-18 17:24:56 +0530

    Async DB initialization on startup

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from app.database import engine
 from app import models
 from app.routers import auth
 
-models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
 
 app.include_router(auth.router)
@@ -12,3 +10,10 @@ app.include_router(auth.router)
 @app.get("/")
 def root():
     return {"message": "FastAPI + PostgreSQL working!"}
+
+# Async DB initialization on startup
+@app.on_event("startup")
+async def init_models():
+    async with engine.begin() as conn:
+        # Run sync create_all inside async engine
+        await conn.run_sync(models.Base.metadata.create_all)
