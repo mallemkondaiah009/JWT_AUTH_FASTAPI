ee75f90a9e9fa895c93360fcdf8a60f40af84ff0
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-12-18 17:25:57 +0530

    async operations added

--- app/database.py
+++ app/database.py
@@ -1,20 +1,25 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Create async engine
+engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+
+# Async session
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
 
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# Dependency
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
